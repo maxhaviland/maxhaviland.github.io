3b4d770e6956ec7e8e66952ac66d30f2ba91fa00
Max Haviland <maxhaviland@gmail.com>
2020-12-06 17:44:19 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  basePath: 'maxhaviland.github.io',
-  assetPrefix: 'maxhaviland.github.io/'
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH+
 }
