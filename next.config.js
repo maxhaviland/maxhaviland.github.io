1a756fe83f7b48f2015446c50aaf514b7a7ada48
Max Haviland <maxhaviland@gmail.com>
2020-12-06 16:03:11 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/maxhaviland/maxhaviland.github.io/main/' : '',
+  basePath: '/docs'
 }
