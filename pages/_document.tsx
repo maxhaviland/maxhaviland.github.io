d5cb4a76247e434b2c69436de74b5636cb906c68
maxhaviland <maxhaviland@gmail.com>
2021-04-02 01:27:16 -0300

    test ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,34 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    const MEASUREMENT_ID = 'G-L8MXKTZ5CN';
-    return (
-      <Html>
-        <Head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: 
-                `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${MEASUREMENT_ID}');`
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		const MEASUREMENT_ID = 'G-L8MXKTZ5CN';
+		return (
+			<Html>
+				<Head>
+					<script async src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}></script>
+					<script
+						data-ad-client="ca-pub-3193469056099321"
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${MEASUREMENT_ID}');`,
+						}}
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
