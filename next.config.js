64fe8843e0d52f491c9e480b0cc19418e8e7f3f2
maxhaviland <maxhaviland@gmail.com>
2021-04-08 16:34:04 -0300

    test webpack config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-	webpack: (config, { dev }) => {
-		config.module.rules = config.module.rules.map((rule) => {
-			if (rule.loader === 'babel-loader') {
-				rule.options.cacheDirectory = false;
-			}
-			return rule;
-		});
-		return config;
-	},
+	// webpack: (config, { dev }) => {
+	// 	config.module.rules = config.module.rules.map((rule) => {
+	// 		if (rule.loader === 'babel-loader') {
+	// 			rule.options.cacheDirectory = false;
+	// 		}
+	// 		return rule;
+	// 	});
+	// 	return config;
+	// },
 };
