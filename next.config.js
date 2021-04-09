9753efab25c44fe698c71d6fb43bac4a50b96ee9
maxhaviland <maxhaviland@gmail.com>
2021-04-08 22:17:23 -0300

    adjustments

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
+const basePath = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_PATH : '';
+
 module.exports = {
-	basePath: 'https://cdn.statically.io/gh/maxhaviland/maxhaviland.github.io/gh-pages/',
-	assetPrefix: 'https://cdn.statically.io/gh/maxhaviland/maxhaviland.github.io/gh-pages/',
-	// webpack: (config, { dev }) => {
-	// 	config.module.rules = config.module.rules.map((rule) => {
-	// 		if (rule.loader === 'babel-loader') {
-	// 			rule.options.cacheDirectory = false;
-	// 		}
-	// 		return rule;
-	// 	});
-	// 	return config;
-	// },
+	basePath,
+	assetPrefix: `${basePath}/`,
+	webpack: (config, { dev }) => {
+		config.module.rules = config.module.rules.map((rule) => {
+			if (rule.loader === 'babel-loader') {
+				rule.options.cacheDirectory = false;
+			}
+			return rule;
+		});
+		return config;
+	},
 };
