34844eb1868fd0f3d14cbc43b44ff3cb82ebb16b
Max Haviland <maxhaviland@gmail.com>
2022-07-31 17:37:45 -0300

    fix: home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
-import { RiWhatsappFill } from 'react-icons/ri';
-import Particles from 'react-particles';
 import { Profile } from '../components/profile';
 import ProjectCard from '../components/projectCard';
 import { projects } from '../data';
