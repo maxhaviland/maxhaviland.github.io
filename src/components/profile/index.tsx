259ba5da6aea4431e51e834a79f306a6721751c7
Max Haviland <64255648+maxhaviland@users.noreply.github.com>
2023-03-03 20:03:11 -0300

    update: profile image

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -5,7 +5,7 @@ import profile from './profile.module.css';
 
 export const Profile: React.FC = () => (
 	<div className={profile.container}>
-		<img className={profile.picture} src="/images/profile.jpg" height={180} width={180} alt="Max Haviland" />
+		<img className={profile.picture} src="/images/profile.jpeg" height={180} width={180} alt="Max Haviland" />
 		<h1>Max Haviland</h1>
 		<ul>
 			<Link href="https://www.linkedin.com/in/max-haviland/">
